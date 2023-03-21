5b20f8fb534ef3694ec4f9872cddcacf08e88deb
Guy Peer <peer.g2@gmail.com>
2023-03-21 17:09:12 +0200

    basic site layout completed

--- frontend/src/cmps/one-shoe-details.jsx
+++ frontend/src/cmps/one-shoe-details.jsx
@@ -1,8 +1,17 @@
+import shoePhoto from '../assets/img/shoe.png'
 
 export function OneShoeDetails() {
     return (
         <section className="one-shoe-details">
-           <h2>details (img+words) section</h2>
+            <img className="shoe-img" src={shoePhoto} />
+            <div className="shoe-info">
+                <span className="type">Cloudsurfer</span>
+                <span className="main-info">
+                    משלבת נחיתות רכות וזינוקים מהירים במיוחד ונותן תחושה של ריצה על עננים. סוליית הגומי המיוחדת שלה מאפשרת ביצועים
+                    משופרים ומצוידת בדפוס מיוחד לאחיזה משופרת של הקרקע כך שתוכל לרוץ מהר ובבטיחות. קימור הנעל נועד במיוחד לריצה כדי
+                    לשמור על תנועת הגל ומהירות גבוהה.
+                </span>
+            </div>
         </section >
     )
 }
--- frontend/src/cmps/one-shoe-suits.jsx
+++ frontend/src/cmps/one-shoe-suits.jsx
@@ -1,8 +1,19 @@
+import logoPhoto from '../assets/img/logo.png'
+import orangePhoto from '../assets/img/orange.png'
+import runnerPhoto from '../assets/img/runner.png'
 
 export function OneShoeSuits() {
     return (
         <section className="one-shoe-suits">
-           <h2>suits to</h2>
+            <img className="logo-img" src={logoPhoto} />
+            <img className="orange-img" src={orangePhoto} />
+            <span className="info-he">:מתאים ל</span>
+
+            <span className="main-txt">ריצות כביש, אימונים, תחרות, ריצות קצרות עד מרתון</span>
+            <img className="orange-img" src={orangePhoto} />
+
+            <span className="video-txt">Video clip</span>
+            <img className="runner-img" src={runnerPhoto} />
         </section >
     )
 }
