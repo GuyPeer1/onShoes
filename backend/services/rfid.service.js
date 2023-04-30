731f56d1daa59a974736f66e0b640ffc6c8ebee3
Guy Peer <peer.g2@gmail.com>
2023-04-30 18:52:32 +0300

    app is now detecting the path to the correct usb port

--- backend/services/rfid.service.js
+++ backend/services/rfid.service.js
@@ -1,8 +1,25 @@
 const { SerialPort } = require('serialport')
 const socketService = require('./socket.service.js')
 
-const FIRST_PORT_PATH = process.env.FIRST_PORT_PATH || (process.platform === 'win32' ? '/COM6' : '/dev/ttyUSB0')
-const SECOND_PORT_PATH = process.env.SECOND_PORT_PATH || (process.platform === 'win32' ? '/COM8' : '/dev/ttyUSB1')
+// const FIRST_PORT_PATH = process.env.FIRST_PORT_PATH || (process.platform === 'win32' ? '/COM6' : '/dev/ttyUSB0')
+// const SECOND_PORT_PATH = process.env.SECOND_PORT_PATH || (process.platform === 'win32' ? '/COM8' : '/dev/ttyUSB1')
+//this is the manufatrer name of our rfid reader
+const MANUFACTURER_NAME = 'Silicon Labs'
+
+//the following function will get the correct path of the rfid's will open its serial port using readRfid func
+getPortPath(MANUFACTURER_NAME)
+
+async function getPortPath(manufacturerName) {
+    const ports = await SerialPort.list()
+    const filteredPorts = ports.filter(port => port.manufacturer === manufacturerName)
+    if (filteredPorts.length === 0) {
+        throw new Error(`No ports found with manufacturer name ${manufacturerName}`)
+    }
+    const FIRST_PORT_PATH = filteredPorts[0].path
+    const SECOND_PORT_PATH = filteredPorts[1].path
+    readRfid(FIRST_PORT_PATH, 'rfid-first')
+    readRfid(SECOND_PORT_PATH, 'rfid-second')
+}
 
 function readRfid(path, eventType) {
     const port = new SerialPort({
@@ -12,7 +29,7 @@ function readRfid(path, eventType) {
     let buffer = Buffer.alloc(0)
 
     port.on('data', data => {
-        buffer = Buffer.concat([buffer, data]);
+        buffer = Buffer.concat([buffer, data])
 
         if (buffer.length >= 16) {
             const tag_id = buffer.slice(3, 15).toString('hex')
@@ -26,11 +43,8 @@ function readRfid(path, eventType) {
             port.close(() => {
                 console.log(`Port ${path} closed`)
                 readRfid(path, eventType)
-            });
+            })
         }
-    });
+    })
 }
-
-readRfid(FIRST_PORT_PATH, 'rfid-first')
-readRfid(SECOND_PORT_PATH, 'rfid-second')
 
