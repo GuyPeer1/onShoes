b1578cb0ad54fb8fa56527e604e26bc9fc26c084
Guy Peer <88733855+GuyPeer1@users.noreply.github.com>
2023-05-01 19:58:18 +0300

    Update linux-start.sh

--- linux-start.sh
+++ linux-start.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="backend" --working-directory="/path/to/backend" -e "npm start"
+echo "Starting servers..."
 
-gnome-terminal --tab --title="frontend" --working-directory="/path/to/frontend" -e "npm start"
+# Change to backend directory
+cd /backend
+echo "Current working directory: $(pwd)"
+
+# Start backend server
+npm start &
+echo "Backend server started."
+
+# Wait for backend server to start
+sleep 10
+
+# Change to frontend directory
+cd ../frontend
+echo "Current working directory: $(pwd)"
+
+# Start frontend server
+npm start
+echo "Frontend server started."
+
+echo "Servers started."
